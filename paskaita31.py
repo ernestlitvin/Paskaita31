93c80cf3aab66b9168c3f42582e4f51e8a0c3d2d
ernestlitvin <erik.litvin@gmail.com>
2025-06-17 13:39:15 +0300

    Doing additional tasks

--- paskaita31.py
+++ paskaita31.py
@@ -236,21 +236,57 @@ for aug in augalai:
         count += 1
 print(count)
 
-print("_______uzd 10_____---------------___")
+print("_______uzd 11_____---------------___")
 print(" ")
 # Sugeneruokite 300 atsitiktinių skaičių nuo 0 iki 300, atspausdinkite juos atskirtus tarpais
 # ir suskaičiuokite kiek tarp jų yra didesnių už 150.
 # Skaičiai didesni nei 275 turi būti atspausdinti skliausteliuose” [ ] “.
 
-rnd_num = random.randint(0,300)
-print(rnd_num, end=" ")
+skaiciai = []
+count = 0
+for _ in range(300):
+    skaicius = random.randint(1,300)
+    skaiciai.append(skaicius)
+    if skaicius > 150:
+        count += 1
 
-for i in random.randint(1,300):
-print((i), end=" ")
+for skaicius in skaiciai:
+    if skaicius > 275:
+        print(f"[{skaicius}]", end=" ")
+    else:
+        print(skaicius, end=" ")
+print()
+print("Number of values greater than 150:", count)
+
+print("___________________")
+
+print("_______uzd 12_____---------------___")
+print(" ")
+# Vienoje eilutėje atspausdinkite visus skaičius nuo 1 iki 3000, kurie dalijasi iš 77 be liekanos.
+# Skaičius atskirkite kableliais. Po paskutinio skaičiaus kablelio neturi būti.
+
+
+results = []
+
+for numbers in range(1,3001):
+    if numbers % 77 == 0:
+        results.append(numbers)
+print(", ".join(str(x) for x in results))
+
+
+print()
+print("_______uzd 13_____---------------___")
+# Nupieškite kvadratą iš “*”, kurio kraštines sudaro 25“*”.
 
 
 
 
+
+
+
+
+
+print(" ")
 
 
 
