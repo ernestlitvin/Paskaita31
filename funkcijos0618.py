9b337f06b41d7c679b6b97ebf350989cd80757dc
ernestlitvin <erik.litvin@gmail.com>
2025-06-18 15:46:34 +0300

    funkcijos 0618 / uzd 6 in progress

--- funkcijos0618.py
+++ funkcijos0618.py
@@ -1,27 +1,44 @@
 # Medziaga
 
-import randomMore actions
-def say_hi():#nieko nepriima, nieko negrazina
+import random
+""""
+def say_hi(): #nieko nepriima, nieko negrazina
     print("sveikuciai")
 say_hi()
+
+def lala():
+    print("hala")
+lala()
+
+
 def say_hi_to(name):#priima kintamaji, nieko negrazina
     print("hi", name)
 say_hi_to("Jonas")
 say_hi_to("Petras")
+
+def lala(vardas):
+    print("hala", vardas)
+lala("ernestas")
+
 vardas = "Klemensas"
-say_hi_to(vardas)
+lala(vardas)
+
+
 
 def sim_pi():#nieko nepriima, grazina reiksme
     return 3.14
 print(sim_pi())
-sp = sim_pi()
-print(sp)
+# sp = sim_pi()
+# print(sp)
+
+
 
 def sumavimas(a,b):#priima du kintamuosius, grazina reiksme
     return a + b
 
-res = sumavimas(4651,6135)
+res = sumavimas(123,5)
 print(res)
+
 
 def make_initials(name,surname):
     return (name[0] + surname[0]).upper()
@@ -70,5 +87,64 @@ txt = generate_rnd_str(10)
 print(txt)
 for i in txt:
     print(i)
+"""
+print()
+print("---------UZD 1-----------")
+# Sukurkite Funkciją kuri priima du kintamuosius, skaičius. Juos susumuoja ir atspausdina.
 
-    asdddd
+def sumavimas(a,b):
+    print (a+b)
+sumavimas(3,7)
+
+print()
+print("---------UZD 2-----------")
+# Sukurkite Funkciją kuri vadinasi PISq. Funkcija gražina reikšmę. Reikšmė yra : 9.8596; Gautą reikšmę atspausdinkite.
+
+def PISq():
+    return 9.8596
+
+print(PISq())
+
+print()
+print("---------UZD 3-----------")
+# Sukurkite Funkciją kuri priima du kintamuosius. Funkcija gražina skaičių sandaugą.; Gautą reikšmę atspausdinkite.
+
+def sandauga(a,b):
+    return a*b
+print(sandauga(3,7))
+
+print()
+print("---------UZD 4-----------")
+# Sukurkite Funkciją kuri priima masyvą, prasuka ciklą ir atspausdina kiekvieną narį vienoje eilutėje.
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+def my_list(lst):
+    for skaiciai in lst:
+        print(skaiciai, end = " ")
+
+my_list(numbers)
+
+print()
+print("---------UZD 5-----------")
+# Sukurkite Funkciją kuri priima du kintamuosius, min ir max reikšmėms nustatyti ir sugeneruoja random int skaičių ir jį gražintų.
+
+
+def skaiciai(min, max):
+    nam = random.randint(min, max)
+    return nam
+
+print(skaiciai(10,100))
+
+print()
+print("---------UZD 6-----------")
+
+# Sukurkite Funkciją kuri sugeneruotų random skaičių masyvą ir jį gražintų.
+# Funkcija priima tris kintamuosius, min, max ir length reikšmėms nustatyti.
+
+def (min,max,length):
+
+    nam = random.randint(min,max)
+    return nam
+
+
